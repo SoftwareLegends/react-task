b2a1c4e7b97d0af42cf9e7231b28c999daba690e
Kodo Yousif <kodo.yousif@gmail.com>
2022-05-31 19:35:30 +0300

    back button implemented to go to home with replacing currect url

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navigation from "./containers/Navigation";
 import Home from "./containers/Home";
 import Adding from "./containers/Adding";
 import { Routes, Route } from "react-router-dom";
+import ContentTitle from "./components/ContentTitle";
 
 export default function App() {
   return (
     <>
       <Navigation />
       <div className="ml-48 w-[calc(100%-12rem)] p-4">
+        <ContentTitle />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/add" element={<Adding />} />
